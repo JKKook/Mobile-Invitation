182e238159ccac2c267999995be8c8b73df55080
JKKook <whdrnrdldia@naver.com>
2023-07-03 14:11:05 +0900

    DOC

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,5 +1,5 @@
 // These styles apply to every route in the application
-import '../styles/global.css';
+import './styles/global.css';
 import Home from './index';
 
 export default function RootLayout({
