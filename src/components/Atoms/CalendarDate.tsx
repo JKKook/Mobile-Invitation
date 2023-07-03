184dfd13719199cb307e3db585bd3ac41604dba9
JKKook <whdrnrdldia@naver.com>
2023-07-03 23:22:00 +0900

    DOC : adjust design of calendar

--- src/components/Atoms/CalendarDate.tsx
+++ src/components/Atoms/CalendarDate.tsx
@@ -1,18 +1,9 @@
-import {
-    format,
-    getDay,
-    startOfMonth,
-    endOfMonth,
-    eachDayOfInterval,
-    differenceInDays,
-} from 'date-fns';
-import { useState } from 'react';
+import { getDay, startOfMonth, endOfMonth, eachDayOfInterval } from 'date-fns';
 
 const CalendarDate = () => {
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-
     // 2023년 10월로 설정 ** 월은 0부터 시작
     const today = new Date(2023, 9, 1);
+    const weekly = ['일', '월', '화', '수', '목', '금', '토'];
     const startOfCurrentMonth = startOfMonth(today);
     const endOfCurrentMonth = endOfMonth(today);
 
@@ -20,12 +11,6 @@ const CalendarDate = () => {
         start: startOfCurrentMonth,
         end: endOfCurrentMonth,
     });
-
-    const daysRemain = differenceInDays(new Date(), new Date(2023, 9, 7));
-
-    // const handleDayClick = (date: Date) => {
-    //     setSelectedDate(date);
-    // };
 
     const getDayClassName = (date: Date) => {
         const day = getDay(date);
@@ -37,17 +22,7 @@ const CalendarDate = () => {
             className += ' text-red-300';
         }
 
-        // else if (day === 6) {
-        //     // 토요일
-        //     className = ' text-blue-200';
-        // }
-
-        // if (date.getDate() === selectedDate?.getDate()) {
-        //     className += ' bg-blue-200';
-        // }
-
         if (date.getDate() === 7) {
-            console.log('date :', date.getDate() === 7);
             className =
                 "scale-125 text-white bg-red-400 hover:bg-red-300 after:content-['🎔']";
         }
@@ -57,8 +32,8 @@ const CalendarDate = () => {
 
     return (
         <div className='select-none max-w-md mx-auto p-4'>
-            <div className='font-kotra grid grid-cols-7 gap-1 mb-2'>
-                {['일', '월', '화', '수', '목', '금', '토'].map((day) => (
+            <div className='font-kotra text-xl grid grid-cols-7 gap-1 mb-2'>
+                {weekly.map((day) => (
                     <div key={day} className='text-center text-gray-600'>
                         {day}
                     </div>
@@ -71,7 +46,6 @@ const CalendarDate = () => {
                         className={`p-2 text-center rounded ${getDayClassName(
                             day,
                         )}`}
-                        // onClick={() => handleDayClick(day)}
                     >
                         {day.getDate()}
                     </div>
